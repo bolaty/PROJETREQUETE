0724e326726720a8bed72b48cbf9c565d2651153
JEAN-JOEL-BLISSE <82817296+JEAN-JOEL-BLISSE@users.noreply.github.com>
2024-02-05 18:25:12 +0000

    save du 05.02.24_3

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,25 +1,25 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { AdminRoutingModule } from "./admin-routing.module";
-import { AdminComponent } from "./admin.component";
-import { AgentComponent } from "./parametrage/agent/agent.component";
-import { ChambresComponent } from "./chambres/chambres.component";
-import { ClientComponent } from "./client/client.component";
-import { ComptabiliteComponent } from "./comptabilite/comptabilite.component";
-import { DashboardComponent } from "./dashboard/dashboard.component";
-import { EditionChambreComponent } from "./edition-etat/edition-chambre/edition-chambre.component";
-import { EditionClientComponent } from "./edition-etat/edition-client/edition-client.component";
-import { EditionComptabiliteComponent } from "./edition-etat/edition-comptabilite/edition-comptabilite.component";
-import { EditionEtatComponent } from "./edition-etat/edition-etat.component";
-import { EditionPrestationsServicesComponent } from "./edition-etat/edition-prestations-services/edition-prestations-services.component";
-import { JourneeExerciceComponent } from "./parametrage/journee-exercice/journee-exercice.component";
-import { PrestationsServiceComponent } from "./prestations-service/prestations-service.component";
-import { RestaurationComponent } from "./restauration/restauration.component";
-import { ExtourneComponent } from "./comptabilite/extourne/extourne.component";
-import { ReglementFactureComponent } from "./comptabilite/reglement-facture/reglement-facture.component";
-import { RecapClientComponent } from "./client/recap-client/recap-client.component";
-import { ReservationComponent } from "./prestations-service/reservation/reservation.component";
-import { AutresPrestationsComponent } from "./prestations-service/autres-prestations/autres-prestations.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AdminRoutingModule } from './admin-routing.module';
+import { AdminComponent } from './admin.component';
+import { AgentComponent } from './parametrage/agent/agent.component';
+import { ChambresComponent } from './chambres/chambres.component';
+import { ClientComponent } from './client/client.component';
+import { ComptabiliteComponent } from './comptabilite/comptabilite.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { EditionChambreComponent } from './edition-etat/edition-chambre/edition-chambre.component';
+import { EditionClientComponent } from './edition-etat/edition-client/edition-client.component';
+import { EditionComptabiliteComponent } from './edition-etat/edition-comptabilite/edition-comptabilite.component';
+import { EditionEtatComponent } from './edition-etat/edition-etat.component';
+import { EditionPrestationsServicesComponent } from './edition-etat/edition-prestations-services/edition-prestations-services.component';
+import { JourneeExerciceComponent } from './parametrage/journee-exercice/journee-exercice.component';
+import { PrestationsServiceComponent } from './prestations-service/prestations-service.component';
+import { RestaurationComponent } from './restauration/restauration.component';
+import { ExtourneComponent } from './comptabilite/extourne/extourne.component';
+import { ReglementFactureComponent } from './comptabilite/reglement-facture/reglement-facture.component';
+import { RecapClientComponent } from './client/recap-client/recap-client.component';
+import { ReservationComponent } from './prestations-service/reservation/reservation.component';
+import { AutresPrestationsComponent } from './prestations-service/autres-prestations/autres-prestations.component';
 import { ParametrageComponent } from './parametrage/parametrage.component';
 import { PlaintesComponent } from './plaintes/plaintes.component';
 import { ReclamationsComponent } from './reclamations/reclamations.component';
@@ -28,6 +28,7 @@ import { AttributionPlaintesReclamationsComponent } from './attribution-plaintes
 import { OperateurComponent } from './operateur/operateur.component';
 import { RelanceComponent } from './relance/relance.component';
 import { SuiviRequeteComponent } from './suivi-requete/suivi-requete.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -59,6 +60,6 @@ import { SuiviRequeteComponent } from './suivi-requete/suivi-requete.component';
     RelanceComponent,
     SuiviRequeteComponent,
   ],
-  imports: [CommonModule, AdminRoutingModule],
+  imports: [CommonModule, AdminRoutingModule, FormsModule],
 })
 export class AdminModule {}
--- src/app/admin/plaintes/plaintes.component.ts
+++ src/app/admin/plaintes/plaintes.component.ts
@@ -1,10 +1,83 @@
 import { Component } from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-plaintes',
   templateUrl: './plaintes.component.html',
-  styleUrls: ['./plaintes.component.scss']
+  styleUrls: ['./plaintes.component.scss'],
 })
 export class PlaintesComponent {
-
+  formulaire_plaintes_reclamations: any = [
+    {
+      id: 'formtabs-num-cpte-clt',
+      type: 'numerique',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'numero de compte client',
+    },
+    {
+      id: 'formtabs-nom-prenoms',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'nom et prénoms',
+    },
+    {
+      id: 'telephone',
+      type: 'numerique',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'téléphone',
+    },
+    {
+      id: 'formtabs-agence',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'agence',
+    },
+    {
+      id: 'formtabs-Localisation',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'localisation',
+    },
+    {
+      id: 'formtabs-type-plainte',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'type de plainte',
+    },
+    {
+      id: 'invoice-description',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'description de la plainte',
+    },
+    {
+      id: 'formtabs-mode-collecte',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'mode de collecte',
+    },
+    {
+      id: 'formtabs-nom-agent',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'nom agent',
+    },
+    {
+      id: 'dropzone-multi-fichier',
+      type: 'text',
+      valeur: '',
+      obligatoire: 'N',
+      label: 'choix de fichier',
+    },
+  ];
 }
