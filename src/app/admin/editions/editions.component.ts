950a2fcd2204ca6edeec6ef2b03ceabfe0887612
jean jo blisse <82817296+JEAN-JOEL-BLISSE@users.noreply.github.com>
2024-02-28 19:23:20 +0000

    save du 28.02.24

--- src/app/admin/editions/editions.component.ts
+++ src/app/admin/editions/editions.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-editions',
   templateUrl: './editions.component.html',
-  styleUrls: ['./editions.component.scss']
+  styleUrls: ['./editions.component.scss'],
 })
 export class EditionsComponent {
+  affichage_etat: boolean = true;
 
+  ConfirmationOptions(action: any) {
+    if (action == 'retour') this.affichage_etat = true;
+    else this.affichage_etat = false;
+  }
 }
