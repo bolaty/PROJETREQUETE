94694bfa75ce55e04b53087677ec9fcb5feb3b98
bolaty <69979488+bolaty@users.noreply.github.com>
2024-04-12 13:25:16 +0000

    save 12/04/2024

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -34,6 +34,19 @@ export class DashboardComponent {
   reclam_dans_le_delai: any = '';
   reclam_hors_le_delai: any = '';
   recap_reclam: any = {};
+  recup_reclamation_stats: any = [];
+  recup_reclamation_enrg: any = [];
+  recup_reclamation_encour: any = [];
+  recup_reclamation_attr: any = [];
+  tab_intermediaire_cercle_absEnrg: any = []
+  tab_intermediaire_cercle_ordEnrgVal: any = [];
+  tab_intermediaire_cercle_absEncour: any = []
+  tab_intermediaire_cercle_ordEncourVal: any = [];
+  tab_intermediaire_cercle_abstraiter: any = []
+  tab_intermediaire_cercle_ordtraiterVal: any = [];
+  couleurs_aleatoiresEnrg: any = [];
+  couleurs_aleatoiresEncour: any = [];
+  couleurs_aleatoirestraiter: any = [];
   tab_intermediaire_cercle_abs: any = [
     'libellé 1',
     'libellé 2',
@@ -90,17 +103,225 @@ export class DashboardComponent {
     },
   };
 
+  FormationChartPieEnregistrer() {
+
+    this.chart_pie = new Chart("MyChartPie3", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels:  this.tab_intermediaire_cercle_absEnrg, // table_label,
+        datasets: [
+          {
+            label: '',
+            data: this.tab_intermediaire_cercle_ordEnrgVal, // table_data,
+            backgroundColor: this.couleurs_aleatoiresEnrg, // table_color,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+  FormationChartPieEnregistrermodal() {
+
+    this.chart_pie = new Chart("MyChartPie33", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels:  this.tab_intermediaire_cercle_absEnrg, // table_label,
+        datasets: [
+          {
+            label: '',
+            data: this.tab_intermediaire_cercle_ordEnrgVal, // table_data,
+            backgroundColor: this.couleurs_aleatoiresEnrg, // table_color,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+
+  FormationChartBarEncour() {
+
+    this.chart_pie = new Chart("MyChartBar2", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels:  this.tab_intermediaire_cercle_absEncour, // table_label,
+        datasets: [
+          {
+            label: '',
+            data: this.tab_intermediaire_cercle_ordEncourVal, // table_data,
+            backgroundColor: this.couleurs_aleatoiresEncour, // table_color,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+
+  FormationChartBarEncourmodal() {
+
+    this.chart_pie = new Chart("MyChartBar22", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels:  this.tab_intermediaire_cercle_absEncour, // table_label,
+        datasets: [
+          {
+            label: '',
+            data: this.tab_intermediaire_cercle_ordEncourVal, // table_data,
+            backgroundColor: this.couleurs_aleatoiresEncour, // table_color,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+
+
+  /*FormationChartBarEncour() {
+    var datacour = []
+    var obj = {
+      label: '',
+      data: '', 
+      backgroundColor: ''
+    }
+    if(this.recup_reclamation_encour.length > 0){
+      for(var i = 0; i < this.recup_reclamation_encour.length; i++){
+        obj = {
+          label: this.recup_reclamation_encour[i].TR_LIBELLETYEREQUETE,
+          data: this.recup_reclamation_encour[i].NOMBRE,
+          backgroundColor: this.couleurs_aleatoiresEncour[i]
+        }
+        datacour.push(obj)
+        obj = {
+          label: '',
+          data: '', 
+          backgroundColor: ''
+        }
+      }
+      
+    }
+    this.chart_bar = new Chart('MyChartBar2', {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels: this.tab_intermediaire_cercle_absEncour, // table_label,
+        datasets: datacour,
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }*/
+
+  /*FormationChartBarEncourmodal() {
+    var datacour = []
+    var obj = {
+      label: '',
+      data: '', 
+      backgroundColor: ''
+    }
+    if(this.recup_reclamation_encour.length > 0){
+      for(var i = 0; i < this.recup_reclamation_encour.length; i++){
+        obj = {
+          label: this.recup_reclamation_encour[i].TR_LIBELLETYEREQUETE,
+          data: this.recup_reclamation_encour[i].NOMBRE,
+          backgroundColor: this.couleurs_aleatoiresEncour[i]
+        }
+        datacour.push(obj)
+        obj = {
+          label: '',
+          data: '', 
+          backgroundColor: ''
+        }
+      }
+      
+    }
+    this.chart_bar = new Chart('MyChartBar22', {
+      type: 'bar', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels: this.tab_intermediaire_cercle_absEncour, // table_label,
+        datasets: datacour,
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }*/
+
+
+
+  FormationChartPietraite() {
+
+    this.chart_pie = new Chart("MyChartPie4", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels:  this.tab_intermediaire_cercle_abstraiter, // table_label,
+        datasets: [
+          {
+            label: '',
+            data: this.tab_intermediaire_cercle_ordtraiterVal, // table_data,
+            backgroundColor: this.couleurs_aleatoirestraiter, // table_color,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+
+  FormationChartPietraitemodal() {
+
+    this.chart_pie = new Chart("MyChartPie44", {
+      type: 'pie', //this denotes tha type of chart
+
+      data: {
+        // values on X-Axis
+        labels:  this.tab_intermediaire_cercle_abstraiter, // table_label,
+        datasets: [
+          {
+            label: '',
+            data: this.tab_intermediaire_cercle_ordtraiterVal, // table_data,
+            backgroundColor: this.couleurs_aleatoirestraiter, // table_color,
+          },
+        ],
+      },
+      options: {
+        aspectRatio: 2.5,
+      },
+    });
+  }
+
   FormationChartPie(id_du_graphe: any) {
     this.chart_pie = new Chart(id_du_graphe, {
       type: 'pie', //this denotes tha type of chart
 
       data: {
         // values on X-Axis
-        labels: this.tab_data_nature_reclam_recu.LIBELLERUBRIQUE, // this.tab_intermediaire_cercle_abs, // table_label,
+        labels:  this.tab_intermediaire_cercle_abs, // table_label,
         datasets: [
           {
             label: '',
-            data: this.tab_data_nature_reclam_recu.NOMBRE, // this.tab_intermediaire_cercle_ord, // table_data,
+            data: this.tab_intermediaire_cercle_ord, // table_data,
             backgroundColor: this.background_color, // table_color,
           },
         ],
@@ -255,21 +476,167 @@ export class DashboardComponent {
       }
     );
   }
-
-  ngOnInit(): void {
-    this.DataDashboard();
-
-    if (!sessionStorage.getItem('isLoggedIn')) {
-      window.location.href = '/auth';
+  DataDashboardSTATISTIQUE() {
+    var TYPEOPERATION = "0";
+    if (this.recupinfo[0].CU_NOMUTILISATEUR.includes('ADMIN')) {
+      TYPEOPERATION = "0"
+    }else if (this.recupinfo[0].TU_CODETYPEUTILISATEUR == '0001'){
+      TYPEOPERATION = "1"
+    }else{
+      TYPEOPERATION = "2"
     }
-    for (let index = 0; index < this.table_id_pie.length; index++) {
-      this.FormationChartPie(this.table_id_pie[index]);
-    }
+    let Option = 'RequeteClientsClasse.svc/pvgChargerDansDataSetDASHBOARD';
+    let body = {
+      Objets: [
+        {
+          OE_PARAM: [this.recupinfo[0].AG_CODEAGENCE,this.recupinfo[0].CU_CODECOMPTEUTULISATEUR,'01/01/1900','01/01/1900',TYPEOPERATION],
+          //OE_PARAM: ['1000','10000000000000000000000000005','01/01/1900','01/01/1900','1'],
+          clsObjetEnvoi: {
+            ET_CODEETABLISSEMENT: '',
+            AN_CODEANTENNE: '',
+            TYPEOPERATION: '0',
+          },
+        },
+      ],
+    };
+    this.recup_reclamation_stats = []
+    this.AdminService.AppelServeur(body, Option).subscribe(
+      (success: any) => {
+        this.tab_data = success;
+        this.tab_data = this.tab_data.pvgChargerDansDataSetDASHBOARDResult;
+        if (this.tab_data[0].clsResultat.SL_RESULTAT == 'TRUE') {
+          this.AdminService.CloseLoader();
+          this.recup_reclamation_stats = this.tab_data
+          this.chargement()
+          this.DataDashboardSTATISTIQUEDETAIL()
+        } else {
+          this.AdminService.CloseLoader();
+          this.recup_reclamation_stats = this.tab_data
+        }
+      },
+      (error) => {
+        this.AdminService.CloseLoader();
+      }
+    );
+  }
 
-    for (let index = 0; index < this.table_id_bar.length; index++) {
-      this.FormationChartBar(this.table_id_bar[index]);
-    }
 
+  DataDashboardSTATISTIQUEDETAIL() {
+    let Option = 'RequeteClientsClasse.svc/pvgTableauDeBordstatistique';
+    var TYPEOPERATION = "ADMIN";
+    if (this.recupinfo[0].CU_NOMUTILISATEUR.includes('ADMIN')) {
+      TYPEOPERATION = "ADMIN"
+    }else if (this.recupinfo[0].TU_CODETYPEUTILISATEUR == '0001'){
+      TYPEOPERATION = "OPERATEUR"
+    }else{
+      TYPEOPERATION = "CLIENT"
+    }
+    let body = {
+      AG_CODEAGENCE: this.recupinfo[0].AG_CODEAGENCE,//'1000',//this.recupinfo[0].AG_CODEAGENCE,//
+      RQ_DATEDEBUT: this.recupinfo[0].EX_DATEDEBUT,//'01/01/1900',//this.recupinfo[0].EX_DATEDEBUT,//
+      RQ_DATEFIN: this.recupinfo[0].EX_DATEFIN,//'01/01/1900',//this.recupinfo[0].EX_DATEFIN,//
+      CU_CODECOMPTEUTULISATEUR: this.recupinfo[0].CU_CODECOMPTEUTULISATEUR,//'10000000000000000000000000005',//this.recupinfo[0].CU_CODECOMPTEUTULISATEUR,//
+      TYPEETAT: TYPEOPERATION,//"OPERATEUR",//TYPEOPERATION,//
+    };
+
+    //this.AdminService.ShowLoader();
+    this.recup_reclamation_enrg = []
+    this.recup_reclamation_encour = []
+    this.recup_reclamation_attr = []
+    this.AdminService.AppelServeur(body, Option).subscribe(
+      (success: any) => {
+        this.tab_data = success;
+        this.tab_data = this.tab_data.pvgTableauDeBordstatistiqueResult;
+        console.log('dashboard_tab_data', this.tab_data);
+        if (this.tab_data.clsResultat.SL_RESULTAT == 'TRUE') {
+          // recuperation des differents tableaux
+          this.tab_data_taux_satisfaction =
+            this.tab_data.clsTableauDeBordTauxSatisfactions;
+          for(let i = 0; i < this.tab_data_taux_satisfaction.length;i++){
+             if(this.tab_data_taux_satisfaction[i].NR_CODENATUREREQUETE == '1'){
+                this.recup_reclamation_enrg.push(this.tab_data_taux_satisfaction[i])
+             }
+             if(this.tab_data_taux_satisfaction[i].NR_CODENATUREREQUETE == '2'){
+                this.recup_reclamation_encour.push(this.tab_data_taux_satisfaction[i])
+             }
+             if(this.tab_data_taux_satisfaction[i].NR_CODENATUREREQUETE == '3'){
+                this.recup_reclamation_attr.push(this.tab_data_taux_satisfaction[i])
+             }
+          }
+          this.tab_intermediaire_cercle_absEnrg = []
+          this.tab_intermediaire_cercle_ordEnrgVal = [];
+          this.tab_intermediaire_cercle_absEncour = []
+          this.tab_intermediaire_cercle_ordEncourVal = [];
+          this.tab_intermediaire_cercle_abstraiter = []
+          this.tab_intermediaire_cercle_ordtraiterVal = [];
+          if(this.recup_reclamation_enrg.length > 0){
+            for(var i=0; i< this.recup_reclamation_enrg.length;i++){
+              this.tab_intermediaire_cercle_absEnrg.push(this.recup_reclamation_enrg[i].TR_LIBELLETYEREQUETE)
+              this.tab_intermediaire_cercle_ordEnrgVal.push(this.recup_reclamation_enrg[i].NOMBRE)
+            }
+          }
+
+          if(this.recup_reclamation_encour.length > 0){
+            for(var i=0; i< this.recup_reclamation_encour.length;i++){
+              this.tab_intermediaire_cercle_absEncour.push(this.recup_reclamation_encour[i].TR_LIBELLETYEREQUETE)
+              this.tab_intermediaire_cercle_ordEncourVal.push(this.recup_reclamation_encour[i].NOMBRE)
+            }
+          }
+
+          if(this.recup_reclamation_attr.length > 0){
+            for(var i=0; i< this.recup_reclamation_attr.length;i++){
+              this.tab_intermediaire_cercle_abstraiter.push(this.recup_reclamation_attr[i].TR_LIBELLETYEREQUETE)
+              this.tab_intermediaire_cercle_ordtraiterVal.push(this.recup_reclamation_attr[i].NOMBRE)
+            }
+          }
+
+
+          // Générer un tableau de couleurs aléatoires de la même taille que tab_intermediaire_cercle_absEnrg
+          this.couleurs_aleatoiresEnrg = []
+          this.couleurs_aleatoiresEncour = []
+          this.couleurs_aleatoirestraiter = []
+          if(this.tab_intermediaire_cercle_absEnrg.length > 0){
+            for (let i = 0; i < this.tab_intermediaire_cercle_absEnrg.length; i++) {
+              let randomIndex = Math.floor(Math.random() * this.background_color.length);
+              this.couleurs_aleatoiresEnrg.push(this.background_color[randomIndex]);
+            }
+          }
+
+          if(this.tab_intermediaire_cercle_absEncour.length > 0){
+            for (let i = 0; i < this.tab_intermediaire_cercle_absEncour.length; i++) {
+              let randomIndex = Math.floor(Math.random() * this.background_color.length);
+              this.couleurs_aleatoiresEncour.push(this.background_color[randomIndex]);
+            }
+          }
+
+          if(this.recup_reclamation_attr.length > 0){
+            for (let i = 0; i < this.recup_reclamation_attr.length; i++) {
+              let randomIndex = Math.floor(Math.random() * this.background_color.length);
+              this.couleurs_aleatoirestraiter.push(this.background_color[randomIndex]);
+            }
+          }
+         
+          this.FormationChartPieEnregistrer()
+          this.FormationChartPieEnregistrermodal()
+          this.FormationChartBarEncour()
+          this.FormationChartBarEncourmodal()
+          this.FormationChartPietraite()
+          this.FormationChartPietraitemodal()
+         
+          //this.AdminService.CloseLoader();
+        } else {
+         // this.AdminService.CloseLoader();
+        }
+      },
+      (error) => {
+       // this.AdminService.CloseLoader();
+      }
+    );
+  }
+
+
+
+  chargement(){
     // chart delai de traitement
     let isDarkStyle = this.isDarkStyle();
     let headingColor;
@@ -295,8 +662,8 @@ export class DashboardComponent {
         },
         labels: ['Reclamations dans les délais', 'Reclamations hors délais'],
         series: [
-          parseInt(this.reclam_dans_le_delai),
-          parseInt(this.reclam_hors_le_delai),
+          parseInt(this.recup_reclamation_stats[0].NOMBREREQUETESTRAITEESDANSDELAIS),
+          parseInt(this.recup_reclamation_stats[0].NOMBREREQUETESTRAITEESHORSDELAIS),
         ],
         colors: [
           this.chartColors.donut.series1,
@@ -336,7 +703,7 @@ export class DashboardComponent {
                   fontWeight: 500,
                   offsetY: -20,
                   formatter: function (val: any) {
-                    return parseInt(val) + 'k';
+                    return parseInt(val) ;
                   },
                 },
                 name: {
@@ -386,6 +753,23 @@ export class DashboardComponent {
       );
       salesOverviewChart.render();
     }
+  }
+
+  ngOnInit(): void {
+    //this.DataDashboard();
+    this.DataDashboardSTATISTIQUE()
+    if (!sessionStorage.getItem('isLoggedIn')) {
+      window.location.href = '/auth';
+    }
+   /* for (let index = 0; index < this.table_id_pie.length; index++) {
+      this.FormationChartPie(this.table_id_pie[index]);
+    }
+
+    for (let index = 0; index < this.table_id_bar.length; index++) {
+      this.FormationChartBar(this.table_id_bar[index]);
+    }*/
+
+    
     // chart delai de traitement
   }
 }
